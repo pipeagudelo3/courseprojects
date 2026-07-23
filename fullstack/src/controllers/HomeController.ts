440d4be4794b7fa814fa86437cc073d8ee9d8ea0
pipeagudelo3 <125285931+pipeagudelo3@users.noreply.github.com>
2026-07-23 09:52:36 -0500

    Completar tutorial Express MPA SSR

---
+++ fullstack/src/controllers/HomeController.ts
@@ -0,0 +1,20 @@
+import type { Request, Response } from 'express';
+
+export class HomeController {
+  static index(req: Request, res: Response): void {
+     const viewData: { [key: string]: any } = {};
+    viewData["title"] = "Home";
+
+    res.render('home/index', { viewData: viewData });
+    res.render('home/index');
+  }
+
+  static about(req: Request, res: Response): void {
+    const viewData: { [key: string]: any } = {};
+    viewData["title"] = "About";
+
+    res.render('home/about', { viewData: viewData });
+    res.render('home/about');
+
+  }
+}
